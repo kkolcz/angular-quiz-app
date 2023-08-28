78d864c30efc47d24d42bf0d57663f33a9de79b7
nietoperz0 <vipciorr@gmail.com>
2023-08-29 01:28:22 +0200

    add quiz working prev next questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { QuizAppComponent } from './quiz-app/quiz-app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuizAppComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, QuizAppComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/quiz-app/quiz-app.component.ts
+++ src/app/quiz-app/quiz-app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-quiz-app',
   templateUrl: './quiz-app.component.html',
-  styleUrls: ['./quiz-app.component.scss']
+  styleUrls: ['./quiz-app.component.scss'],
 })
-export class QuizAppComponent {
+export class QuizAppComponent implements OnInit {
+  questionsList: any = [{}];
+  questionNumber: number = 1;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadQuestions();
+  }
+
+  loadQuestions() {
+    this.http.get('http://localhost:3000/questions').subscribe((res) => {
+      this.questionsList = res;
+      console.log(this.questionsList);
+    });
+  }
+
+  nextQuestion() {
+    if (this.questionNumber < this.questionsList.length) {
+      this.questionNumber++;
+    }
+  }
+
+  prevQuestion() {
+    if (this.questionNumber > 1) {
+      this.questionNumber--;
+    }
+  }
 }
