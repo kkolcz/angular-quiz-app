6287fe3d21bffe173a7f1594972759993c84d08c
kkolcz <vipciorr@gmail.com>
2024-01-25 03:34:57 +0100

    preparing config file

--- src/app/environments/enviroments.ts
+++ src/app/environments/enviroments.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: 'false',
-  API_URL: 'https://quiz-angular-55f08-default-rtdb.firebaseio.com',
+  API_URL: '',
   firebase: {
-    apiKey: 'AIzaSyBcu8kSv8-9rwAnB3i572oTwWXL28gxM3k',
-    authDomain: 'angular-learning-51cd7.firebaseapp.com',
-    projectId: 'angular-learning-51cd7',
-    storageBucket: 'angular-learning-51cd7.appspot.com',
-    messagingSenderId: '872740714834',
-    appId: '1:872740714834:web:43fc4d75e10e306c0ea28b',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
 };
