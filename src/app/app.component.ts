bcacd195d6729fecfe0260557aaba060c99953f8
kkolcz <vipciorr@gmail.com>
2024-01-25 06:02:15 +0100

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,42 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  // title = 'angular-quiz-app';
-  // username: string = 'unknown';
-  // category: string = '';
-
-  // quizStarted = false;
-  // quizEnd = false;
-  // points: number = 0;
-
-  // showWelcome: boolean = true;
-
-  // closeWelcome() {
-  //   this.showWelcome = false;
-  // }
-
-  // startQuiz(val: boolean) {
-  //   this.quizStarted = val;
-  // }
-
-  // submitQuiz(points: number) {
-  //   this.points = points;
-  //   this.quizEnd = true;
-  // }
-
-  // setUsername(username: string) {
-  //   this.username = username;
-  // }
-
-  // setCategory(category: string) {
-  //   this.category = category;
-  // }
-
-  // restartQuiz() {
-  //   this.quizStarted = false;
-  //   this.quizEnd = false;
-  //   this.points = 0;
-  //   // console.log('restarttttt');
-  // }
-}
+export class AppComponent {}
